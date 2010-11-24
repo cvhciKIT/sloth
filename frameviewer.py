593647885bc06981390750894c01967097aed94e
Mika Fischer <mika.fischer@kit.edu>
2010-11-24 13:44:45 +0100

    Make QGraphicsView scrollable by mouse drag for now

--- frameviewer.py
+++ frameviewer.py
@@ -16,6 +16,7 @@ class GraphicsView(QGraphicsView):
 
     def __init__(self, parent=None):
         QGraphicsView.__init__(self, parent)
+        self.setDragMode(QGraphicsView.ScrollHandDrag)
         self.setRenderHint(QPainter.Antialiasing)
         self.setRenderHint(QPainter.TextAntialiasing)
         self.setStyleSheet("QFrame { border: 3px solid black }");
