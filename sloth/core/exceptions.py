bc25b1227a0cbd557f722327a4c7d6cb7a924016
Martin Baeuml <baeuml@kit.edu>
2011-05-17 14:12:01 +0200

    add NotImplementedException

--- sloth/core/exceptions.py
+++ sloth/core/exceptions.py
@@ -5,3 +5,7 @@ Label tool exception classes.
 class ImproperlyConfigured(Exception):
     """There is an error in the configuration."""
     pass
+
+class NotImplementedException(Exception):
+    """This function/method/class has not been implemented yet."""
+    pass
