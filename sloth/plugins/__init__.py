aa1ba7888d8e69f4a2712ed16ddd4dde82a2a0e0
Jens Koenig <jens.koenig@mailbox.org>
2016-12-03 13:17:14 +0100

    removed enumerate polygon plugin

--- sloth/plugins/__init__.py
+++ sloth/plugins/__init__.py
@@ -6,25 +6,4 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
-class PolygonEnumeratorPlugin(QObject):
-    """Enumerate the corners of polygons."""
 
-    def __init__(self, labeltool):
-        QObject.__init__(self)
-
-        # Decorate the paint() method with our enumerating paint:
-        from sloth.items import PolygonItem
-        oldpaint = PolygonItem.paint
-
-        def paint(self, painter, option, widget=None):
-            oldpaint(self, painter, option, widget)
-            for i, p in enumerate(self._polygon):
-                painter.drawText(p, str(i))
-
-        import functools
-        functools.update_wrapper(paint, oldpaint)
-
-        PolygonItem.paint = paint
-
-    def action(self):
-        return None
